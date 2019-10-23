8c6da8ba47da7165533bf783217b34bc8475693c
Gracjan Jasiowka <gracjan.jasiowka@globallogic.com>
2019-10-23 07:34:29 +0200

    Initial commit

---
+++ ProjektInżynierski/ProjektInżynierski/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjektInżynierski.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
